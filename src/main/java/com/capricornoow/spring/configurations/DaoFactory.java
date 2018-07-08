f27d086d5692eba91e07b2ac0f8ee7d7458fa27f
Ssurey Moon <ssureymoon@gmail.com>
2018-07-08 17:22:35 +0900

    [vol.1-chap.1] add simple user DAO and connectionMaker injection

---
+++ src/main/java/com/capricornoow/spring/configurations/DaoFactory.java
@@ -0,0 +1,9 @@
+package com.capricornoow.spring.configurations;
+
+import com.capricornoow.spring.daos.ConnectionMaker;
+import com.capricornoow.spring.daos.DConnectionMaker;
+import com.capricornoow.spring.daos.UserDao;
+
+public class DaoFactory {
+
+}
---
+++ src/test/java/com/capricornoow/spring/daos/UserDaoTest.java
@@ -0,0 +1,37 @@
+package com.capricornoow.spring.daos;
+
+import com.capricornoow.spring.domain.User;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+
+/**
+ * Created by smoon on 2018. 7. 8..
+ */
+public class UserDaoTest {
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+
+        User user = new User();
+        user.setId("whiteship1");
+        user.setName("나유저");
+        user.setPassword("passpass");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+        assertThat(user.getName(), is("나유저"));
+
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName() + " 조회 성공");
+        assertThat(user.getName(), is(user2.getName()));
+
+        userDao.delete(user.getId());
+    }
+}
