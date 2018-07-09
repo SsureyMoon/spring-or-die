01c9db0311ee3c073e5226281ee02af433a13db0
Ssurey Moon <ssureymoon@gmail.com>
2018-07-09 21:38:43 +0900

    [vol.1-chap.2] improve UserDao test case - add unknown id exception case - add count test for multi users - improve get user by id test for multi users

--- src/test/java/com/capricornoow/spring/daos/UserDaoTest.java
+++ src/test/java/com/capricornoow/spring/daos/UserDaoTest.java
@@ -4,6 +4,7 @@ import com.capricornoow.spring.domain.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -19,19 +20,64 @@ public class UserDaoTest {
         userDao.deleteAll();
         assertThat(userDao.getCount(), is(0));
 
-        User user = new User();
-        user.setId("whiteship1");
-        user.setName("나유저");
-        user.setPassword("passpass");
+        User user1 = new User("whiteship1", "나유저", "passpass1");
+        User user2 = new User("whiteship2", "너도유저", "passpass2");
 
-        userDao.add(user);
-        assertThat(userDao.getCount(), is(1));
+        userDao.add(user1);
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
 
-        User user2 = userDao.get(user.getId());
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        User user1get = userDao.get(user1.getId());
+        assertThat(user1get.getName(), is(user1.getName()));
+        assertThat(user1get.getPassword(), is(user1.getPassword()));
+
+        User user2get = userDao.get(user2.getId());
+        assertThat(user2get.getName(), is(user2.getName()));
+        assertThat(user2get.getPassword(), is(user2.getPassword()));
 
         userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
+    }
+
+    @Test(expected=EmptyResultDataAccessException.class)
+    public void getUserFailure() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
+
+        userDao.get("unknown_id");
+    }
+
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("id1", "일이삼", "pass1");
+        User user2 = new User("id2", "사오육", "pass2");
+        User user3 = new User("id3", "칠팔구", "pass3");
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
+
+        userDao.add(user1);
+        assertThat(userDao.getCount(), is(1));
+
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
+
+        userDao.add(user3);
+        assertThat(userDao.getCount(), is(3));
+
+        userDao.delete(user3.getId());
+        assertThat(userDao.getCount(), is(2));
+
+        userDao.delete(user2.getId());
+        assertThat(userDao.getCount(), is(1));
+
+        userDao.delete(user1.getId());
         assertThat(userDao.getCount(), is(0));
     }
 }
