f3068978f9b1ffdb766222974f7ede69c12fb188
Ssurey Moon <ssureymoon@gmail.com>
2018-07-22 23:34:17 +0900

    [vol.1-chap.3] use spring native JdbcTemplate for querying results

--- src/main/java/com/capricornoow/spring/daos/UserDao.java
+++ src/main/java/com/capricornoow/spring/daos/UserDao.java
@@ -1,22 +1,16 @@
 package com.capricornoow.spring.daos;
 
 import com.capricornoow.spring.domain.User;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
     }
 
     public void add(final User user) throws SQLException {
@@ -28,46 +22,14 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        User user = null;
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement(
-                    "select * from users where id = ?");
-            ps.setString(1, id);
-
-            rs = ps.executeQuery();
-            if(rs.next()) {
-                user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-            }
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if(rs != null){
-                try {
-                    rs.close();
-                } catch (SQLException e) {}
-            }
-            if(ps != null){
-                try {
-                    ps.close();
-                } catch (SQLException e) {}
-
-            }
-            if(conn != null){
-                try {
-                    conn.close();
-                } catch (SQLException e) {}
-            }
-        }
-        if(user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, (rs, rowNum) -> {
+            // 이미 첫번째 row를 가리키고 있어서 rs.next 호출 필요 없다.
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        });
     }
 
     public void delete(final String id) throws SQLException {
@@ -79,36 +41,6 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement(
-                    "select count(*) as count from users");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt("count");
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if(rs != null){
-                try {
-                    rs.close();
-                } catch (SQLException e) {}
-            }
-            if(ps != null){
-                try {
-                    ps.close();
-                } catch (SQLException e) {}
-
-            }
-            if(conn != null){
-                try {
-                    conn.close();
-                } catch (SQLException e) {}
-            }
-        }
+        return this.jdbcTemplate.queryForObject("select count(*) as count from users", Integer.class);
     }
 }
