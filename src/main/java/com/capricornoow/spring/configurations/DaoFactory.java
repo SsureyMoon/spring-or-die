3a42a513e351d43f7a7474ac4792457a0d51212b
Ssurey Moon <ssureymoon@gmail.com>
2018-08-05 21:27:00 +0900

    remove unused connection couting userDao

--- src/main/java/com/capricornoow/spring/configurations/DaoFactory.java
+++
@@ -1,9 +0,0 @@
-package com.capricornoow.spring.configurations;
-
-import com.capricornoow.spring.daos.ConnectionMaker;
-import com.capricornoow.spring.daos.DConnectionMaker;
-import com.capricornoow.spring.daos.UserDao;
-
-public class DaoFactory {
-
-}
--- src/main/java/com/capricornoow/spring/daos/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-package com.capricornoow.spring.daos;
-
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
